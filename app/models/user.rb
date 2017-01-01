1a21e12b43679be5ffc11cb31882386a2b9bf585
余婷 <superyuting2016@gmail.com>
2017-01-01 12:50:20 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :groups
   has_many :posts
   has_many :group_relationships
@@ -10,5 +11,13 @@ class User < ApplicationRecord
 
   def is_member_of?(group)
 	participated_groups.include?(group)
+  end
+
+  def join!(group)
+  	participated_groups << group
+  end
+
+  def quit!(group)
+  	participated_groups.delete(group)
   end
 end
